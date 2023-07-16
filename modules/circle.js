f63f46a1bf350357418a7d86e6aea08c984f9983
Anil Phiyak <phiyakanil@gmail.com>
2023-07-16 08:47:44 +0530

    Day-2: understanding nodeJs insides

---
+++ modules/circle.js
@@ -0,0 +1,41 @@
+const { PI } = Math;
+
+console.log("circle module is loaded")
+const area = (r) => PI * r ** 2;
+const circumference = (r) => 2 * PI * r;
+
+console.log("Current:",require.main === module )
+
+
+
+// Approach : 3
+// module.exports = exports ={
+//     area, circumference
+// }
+// exports= {} is not bound to module.exports 
+
+
+
+// Approach : 2
+// module.exports ={
+//     area,
+//     circumference
+// }
+
+
+// Approach :1
+// exports.area = area;
+// exports.circumference =  circumference;
+
+
+const main = require.main;
+if (main === module) {
+  console.log('The current module is the main module.');
+} else {
+  console.log('The current module is not the main module.');
+}
+// This snippet will print The current module is the main module. 
+// if the file is run from the command line, and 
+// The current module is not the main module. 
+// if it is required from another module.
+
---
+++ modules/foo.js
@@ -0,0 +1,6 @@
+const check = require('./circle.js');
+
+// console.log("check",check)
+console.log("Current:",require.main === module )
+
+console.log(`The area of a circle of radius 4 is ${area(4)}`);
